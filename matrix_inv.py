cfbf1bd6e24186648885c17d5ad2308f6b374d4d
Vegetable42 <149073310+Vegetable42@users.noreply.github.com>
2024-10-07 16:07:58 +0200

    Update matrix_inv.py

--- matrix_inv.py
+++ matrix_inv.py
@@ -64,51 +64,42 @@ for i in range(0,2):
 with open('output.txt', 'w') as file:
      for i in range(0,2):
          for j in range (0,9):
-             print("____________________________________________", file = file)
-             print ("\n\n\nBIN   ",i, " ", j, file = file)
-             print("Rich matrix:", file = file)
-             print(np.transpose(mat_dict[(i,j)]), file = file)      
+             #print("____________________________________________", file = file)
+             print ("BIN ",i, " ", j, file = file)
+             #print("Rich matrix:", file = file)
+             #print(np.transpose(mat_dict[(i,j)]), file = file)      
             
-             print("\nInverse matrix:", file = file)
+             #print("Inverse matrix:", file = file)
              
              if (np.linalg.det(mat_dict[(i,j)])>1e-10):
                   inv_dict[(i,j)] = np.linalg.inv(mat_dict[(i,j)])
-                  print(inv_dict[(i,j)], file = file)     
+                  #print(np.transpose(inv_dict[(i,j)]), file = file)   
+                  for s in range(0,3):
+                          m = 0;
+                          print(inv_dict[(i,j)][m, s], " ", inv_dict[(i,j)][m+1, s], " ", inv_dict[(i,j)][m+2, s], file = file)
              else:
                  print("MATRIX IS SINGULAR", file = file)
             
 #             print("\nDeterminant", file = file)
 #             print(np.linalg.det(mat_dict[(i,j)]), file = file)  
 
-compare =True
+compare = True
 tbin = 0
 pbin = 0
 
 inv_diff = {}
-
-for i in range(0,2):
-    for j in range (0,9):
-        inv_diff[(i,j)] = np.zeros(shape=(3,3))
-        
 mat_diff = {}
-
-for i in range(0,2):
-    for j in range (0,9):
-        mat_diff[(i,j)] = np.zeros(shape=(3,3))
-
+inv_dav = {}
 mat_dav = {}
 
 for i in range(0,2):
     for j in range (0,9):
+        inv_diff[(i,j)] = np.zeros(shape=(3,3))
+        inv_dav[(i,j)] = np.zeros(shape=(3,3))
+        mat_diff[(i,j)] = np.zeros(shape=(3,3))
         mat_dav[(i,j)] = np.zeros(shape=(3,3))
         
-inv_dav = {}
-
-for i in range(0,2):
-    for j in range (0,9):
-        inv_dav[(i,j)] = np.zeros(shape=(3,3))
-
-
+#Comparison of mine and Davides values
 if compare == True:
     with open('rich_inv.txt', 'r') as file:
         # Read all lines from the file
@@ -120,14 +111,15 @@ if compare == True:
             if ('#' in line and k > 0):
                 pbin += 1
                 
-                if (pbin == 8):
-                    pbin = 0
-                    tbin += 1
+            #number of pbins reached
+            if (pbin == 9):
+                pbin = 0
+                tbin += 1
             
-            if (tbin == 3 and pbin == 0):
+            if (tbin == 2 and pbin == 0):
                 break
             
-            if (tbin != 3 and np.linalg.det(mat_dict[(tbin,pbin)]) < 0.001):
+            if (tbin != 2 and np.linalg.det(mat_dict[(tbin,pbin)]) < 0.001):
                 print("NULL DETERMINANT   ", tbin, "   ", pbin)
                 continue
             
@@ -161,41 +153,129 @@ if compare == True:
                             # if (abs(float(split[i])-(inv_dict[(tbin,pbin)][i][j])) > 0.01):
                             #     print(tbin," ", pbin, " ", i, " ", j, " ",abs(float(split[i])-inv_dict[(tbin,pbin)][i][j]))
 
-with open('output.txt', 'w') as file:
+# with open('output.txt', 'w') as file:
+#     for i in range(0,2):
+#         for j in range (0,9):
+#             print("AAAAAAa", file = file)
+#             if (i==2 and j == 7 ):
+#                 continue
+            
+#             print("____________________________________________", file = file)
+#             print ("\nBIN   ",i, " ", j,"\n", file = file)
+#             print("Rich matrix differences:\n", file = file)
+            
+#             print("\nMy matrix:\n",mat_dict[(i,j)],"\n\n", file = file)
+            
+#             print("\nDavide's matrix:\n",mat_dav[(i,j)],"\n\n", file = file)
+#             print("\nDifference:\n",file=file)
+#             print(mat_diff[(i,j)], file = file)      
+            
+#             for s in range(0,2):
+#                 for t in range (0,9):
+#                     if (mat_diff[(i,j)][s][t]>0.0001):
+#                         print("\nDifferent on [", s, ", ",t,"]",", the difference = ", mat_diff[(i,j)][s][t], file = file)
+            
+#             print("\nInverse matrix differences:\n", file = file)
+            
+#             print("\nMy matrix:\n",inv_dict[(i,j)],"\n\n", file = file)
+            
+#             print("\nDavide's matrix:\n",inv_dav[(i,j)],"\n\n", file = file)
+#             print("\nDifference:\n",file=file)
+#             print(inv_diff[(i,j)], file = file)     
+            
+#             for s in range(0,2):
+#                 for t in range (0,9):
+#                     if (inv_diff[(i,j)][s][t]>1e-5):
+#                         print("\nDifferent on [", s, ", ",t,"]",", the difference = ", inv_diff[(i,j)][s][t], file = file)
+            
+
+with open ('counts.txt', 'r') as file:
+    
+    counts_plus = {}
+    counts_minus = {}
+    actual = "p+"
     for i in range(0,2):
         for j in range (0,9):
+            counts_plus[(i,j)] = np.zeros(3)
+            counts_minus[(i,j)] = np.zeros(3)
+
+    # Read all lines from the file
+    lines = file.readlines()
+    for k in range(len(lines)):
+        line = lines[k]
+        
+        s = line.split()
+        
+        
+        if (len(s) == 1):
+            print(s)
+            if (s[0] == "p+"):
+                actual = s[0]
+            if (s[0] == "p-"):
+                actual = s[0]                
+            if (s[0] == "k+"):
+                actual = s[0] 
+            if (s[0] == "k-"):
+                actual = s[0]
+            if (s[0] == "pi+"):
+                actual = s[0]
+            if (s[0] == "pi-"):
+                actual = s[0]
+        
+        elif (len(s) == 3):
+            theta_bin = float(s[0])
+            p_bin = float(s[1])
             
-            if (i==2 and j == 7 ):
-                continue
+            if (actual == "pi+"):
+                counts_plus[(theta_bin, p_bin)][0] = float(s[2])
+                
+            elif (actual == "k+"):
+                counts_plus[(theta_bin, p_bin)][1] = float(s[2])
+                
+            elif (actual == "p+"):
+                counts_plus[(theta_bin, p_bin)][2] = float(s[2])
+                           
+            elif (actual == "pi-"):
+                counts_minus[(theta_bin, p_bin)][0] = float(s[2])
+                
+            elif (actual == "k-"):
+                counts_minus[(theta_bin, p_bin)][1] = float(s[2])
+                
+            elif (actual == "p-"):
+                counts_minus[(theta_bin, p_bin)][2] = float(s[2])
             
-            print("____________________________________________", file = file)
-            print ("\nBIN   ",i, " ", j,"\n", file = file)
-            print("Rich matrix differences:\n", file = file)
-            
-            print("\nMy matrix:\n",mat_dict[(i,j)],"\n\n", file = file)
-            
-            print("\nDavide's matrix:\n",mat_dav[(i,j)],"\n\n", file = file)
-            print("\nDifference:\n",file=file)
-            print(mat_diff[(i,j)], file = file)      
-            
-            for s in range(0,2):
-                for t in range (0,9):
-                    if (mat_diff[(i,j)][s][t]>0.0001):
-                        print("\nDifferent on [", s, ", ",t,"]",", the difference = ", mat_diff[(i,j)][s][t], file = file)
-            
-            print("\nInverse matrix differences:\n", file = file)
-            
-            print("\nMy matrix:\n",inv_dict[(i,j)],"\n\n", file = file)
-            
-            print("\nDavide's matrix:\n",inv_dav[(i,j)],"\n\n", file = file)
-            print("\nDifference:\n",file=file)
-            print(inv_diff[(i,j)], file = file)     
-            
-            for s in range(0,2):
-                for t in range (0,9):
-                    if (inv_diff[(i,j)][s][t]>1e-5):
-                        print("\nDifferent on [", s, ", ",t,"]",", the difference = ", inv_diff[(i,j)][s][t], file = file)
-            
-print(abs(1.58876e-2-0.0158876))
+    print(counts_plus)
+    print(counts_minus)        
     
     
+with open('multiplication.txt', 'w') as file:
+    result_plus = {}
+    result_minus = {}
+    
+    for i in range(0,2):
+        for j in range (0,9):      
+            
+            print(i," ",j, file=file)
+            print("PLUS", file = file)
+            print (np.matmul(np.transpose(inv_dict[(i,j)]), counts_plus[(i,j)]), file = file)
+            
+            print("MINUS", file = file)
+            print (np.matmul(np.transpose(inv_dict[(i,j)]), counts_minus[(i,j)]), file = file)
+            print(file=file)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
